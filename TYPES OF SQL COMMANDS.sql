566847d84c2752cc6988d33a957af2e1571689ba
VIGNESH-SEKAR97 <vignesh97sekar@gmail.com>
2026-01-17 20:40:06 +0530

    Add files via upload

---
+++ TYPES OF SQL COMMANDS.sql
@@ -0,0 +1,43 @@
+-- TYPES OF SQL COMMANDS
+/*
+1. DDL Data Definition Language
+2. DML Data Manipulation Language
+3. DQL Data Query Language
+4. DCL Data Control Language
+5. TCL Transaction Control Language
+*/
+-- DDL Data Definition Language
+/*
+Used to define or modify database structure.
+Common DDL Commands:
+CREATE – Create database/table
+ALTER – Modify table structure
+DROP – Delete database/table
+TRUNCATE – Remove all records from a table
+RENAME – Rename a database object
+*/
+
+-- DQL Data Query Language
+/*
+Used to retrieve data from the database.
+Common DQL Commands:
+SELECT – Fetch data from tables
+*/
+
+-- DCL Data Control Language
+/*
+Used to control access and permissions.
+Common DCL Commands:
+GRANT – Give user privileges
+REVOKE – Remove user privileges
+*/
+
+-- TCL Transaction Control Language
+/*
+Used to manage transactions in the database.
+
+Common TCL Commands:
+COMMIT – Save changes permanently
+ROLLBACK – Undo changes
+SAVEPOINT – Set a point to roll back to
+*/
